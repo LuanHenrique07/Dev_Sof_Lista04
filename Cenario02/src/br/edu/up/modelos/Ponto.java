ce122a59342be3073a7afd3ed7a2bb993c24a226
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-04-15 21:35:34 -0300

    Inicio do Objeto Ponto

---
+++ Cenario02/src/br/edu/up/modelos/Ponto.java
@@ -0,0 +1,21 @@
+package br.edu.up.modelos;
+
+public class Ponto {
+    public double x;
+    public double  y;
+    
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    
+}
