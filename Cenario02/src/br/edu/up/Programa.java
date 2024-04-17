1031e5cfce25b74589f4bc61df76f22eadd21c05
gabriel <gzanella202@gmail.com>
2024-04-17 20:45:54 -0300

    Cenario02 Teste

--- Cenario02/src/br/edu/up/Programa.java
+++ Cenario02/src/br/edu/up/Programa.java
@@ -1,9 +1,22 @@
 package br.edu.up;
 
+import br.edu.up.modelos.Ponto;
+
 public class Programa {
-    public static void main(String[] args) throws Exception {
-     public Ponto {
+    public static void main(String[] args){
+
+        Ponto ponto1 = new Ponto();
+        ponto1.x = 0;
+        ponto1.y = 0;
         
-     }
+        Ponto ponto2 = new Ponto();
+        ponto2.x = 2;
+        ponto2.y = 5;
+        
+        
+        
+        System.out.println(ponto1.calcularDistania(ponto1, ponto2));
+        
     }
+  
 }
--- Cenario02/src/br/edu/up/modelos/Ponto.java
+++ Cenario02/src/br/edu/up/modelos/Ponto.java
@@ -17,5 +17,12 @@ public class Ponto {
         this.y = y;
     }
 
+    public double calcularDistania(Ponto ponto1, Ponto ponto2 ){
+        double distancia = Math.pow(Math.sqrt(ponto1.x - ponto2.x), 2) 
+        + Math.pow(Math.sqrt(ponto1.y - ponto2.y), 2);
+
+        return distancia;
+    }
+
     
 }
