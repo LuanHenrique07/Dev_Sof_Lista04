2a0cd18900c8c38b5861812078e9b29d4f5ffacc
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-04-21 21:42:23 -0300

    Mudancas no Cenario 2

--- Cenario02/src/br/edu/up/Programa.java
+++ Cenario02/src/br/edu/up/Programa.java
@@ -5,9 +5,29 @@ import br.edu.up.modelos.Ponto;
 public class Programa {
     public static void main(String[] args){
 
-        Ponto ponto1 = new Ponto(2, 3);
-        Ponto ponto2 = new Ponto(7, 8);
-        double distancia = ponto1.calcularDistancia(ponto2);
-        System.out.println(distancia);
+        // Pedidos 1 e 2 do Cenario 2
+        Ponto ponto1 = new Ponto(0, 0);
+        Ponto ponto2 = new Ponto(2, 5);
+
+        // Pedido 3 
+        double d1 = ponto1.calcularDistancia(ponto2);
+
+        System.out.println("A distancia entre o ponto1 e o ponto2 eh: " + d1);
+
+        // Pedido 4
+        Ponto ponto3 = new Ponto(7, 2);
+
+        double d2 = ponto2.calcularDistancia(ponto3);
+
+        System.out.println("A distancia entre o ponto2 e o ponto3 eh: " + d2);
+
+        // Pedidos 5 e 6
+        ponto1.setX(10);
+        ponto1.setY(3);
+
+        double d3 = ponto1.calcularDistancia(ponto2);
+
+        System.out.println("A distancia entre o ponto1 alterado e o ponto2 eh: " + d3);
+
     }
 }
--- Cenario02/src/br/edu/up/modelos/Ponto.java
+++ Cenario02/src/br/edu/up/modelos/Ponto.java
@@ -2,15 +2,33 @@ package br.edu.up.modelos;
 
 public class Ponto {
     public double x;
-    public double  y;
+    public double y;
+    public double d;
 
-    public Ponto(double x, double y) {
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Ponto(double x, double y){
         this.x = x;
         this.y = y;
     }
 
-    public double calcularDistancia(Ponto ponto2) {
-        return Math.sqrt(Math.pow(ponto2.x - this.x, 2) + Math.pow(ponto2.y - this.y, 2));
+    public double calcularDistancia(Ponto outroPonto) {
+        d = Math.sqrt(Math.pow(this.x - outroPonto.getX(), 2) + Math.pow(this.y - outroPonto.getY(), 2));
+        return d;
     }
 }
 
