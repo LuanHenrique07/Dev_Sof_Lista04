d5c175c3c6ec60637aed046ae319eec055fd2b1f
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-04-22 20:36:12 -0300

    Getters e setters classe dia

--- Cenario03/src/br/edu/up/modelos/Dia.java
+++ Cenario03/src/br/edu/up/modelos/Dia.java
@@ -1,7 +1,39 @@
 package br.edu.up.modelos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dia {
     public int diaMes;
     public Compromisso[] compromissos;
+    List<String> listaCompro = new ArrayList<>();
+
+    public Dia(int diaMes){
+        this.diaMes = diaMes;
+    }
+
+    public void adicionarCompromisso(Compromisso comp){
+        
+    }
     
+    public static void consultarCompromisso(int hora){
+
+    }
+
+    public void excluirCompromisso(int hora){
+
+    }
+
+    public static void listarCompromissos(){
+    }
+
+    public List<String> getListaCompro() {
+        return listaCompro;
+    }
+
+    public void setListaCompro(List<String> listaCompro) {
+        this.listaCompro = listaCompro;
+    } 
+    
+
 }
