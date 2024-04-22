a481ddfc4172ac07aaf36b16cf5bef807b7afde2
gabriel <gzanella202@gmail.com>
2024-04-22 20:43:32 -0300

    Continuacao Mes

--- Cenario03/src/br/edu/up/modelos/Mes.java
+++ Cenario03/src/br/edu/up/modelos/Mes.java
@@ -5,4 +5,28 @@ public class Mes {
     public int qtdeDias;
     public Dia[] dias;
 
+    public Mes(String nome, int qtdeDias){
+        this.nome = nome;
+        this.qtdeDias = qtdeDias;
+    }
+
+    public void adicionarCompromisso(Compromisso comp, int diaMes){
+        
+    }
+
+    public void adicionarCompromisso(String pessoa, String local, String assunto, int hora, int diaMes){
+        
+    }
+
+    public void excluirCompromisso(int diaMes, int hora){
+
+    }
+
+    public static void listaCompromissos(int diaMes){
+
+    }
+
+    public static void listaCompromisso(){
+        
+    }
 }
