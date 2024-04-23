d36e57ab2b1637470125f3f2dd2a9398c43125f8
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-04-22 21:36:12 -0300

    Mudanças

--- Cenario03/src/br/edu/up/Programa.java
+++ Cenario03/src/br/edu/up/Programa.java
@@ -1,7 +1,11 @@
 package br.edu.up;
 
+import br.edu.up.modelos.Compromisso;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-        
+        Compromisso futebol = new Compromisso("Zanella", "AVN", "Bola", 14);
+
+        System.out.println(futebol.getLocal());
     }
 }
--- Cenario03/src/br/edu/up/modelos/Dia.java
+++ Cenario03/src/br/edu/up/modelos/Dia.java
@@ -1,37 +1,42 @@
 package br.edu.up.modelos;
 
+
 import java.util.ArrayList;
 import java.util.List;
+
+import br.edu.up.Prompt;
 
 public class Dia {
     public int diaMes;
     public Compromisso[] compromissos;
-    List<String> listaCompro = new ArrayList<>();
+    public static List<Compromisso> listaCompro = new ArrayList<>();
 
     public Dia(int diaMes){
         this.diaMes = diaMes;
     }
 
     public void adicionarCompromisso(Compromisso comp){
-        
+        this.listaCompro.add(comp);
     }
     
     public static void consultarCompromisso(int hora){
-
+        for ( var compromisso : listaCompro) {
+            Prompt.imprimir(compromisso);
+        }
     }
 
     public void excluirCompromisso(int hora){
-
+        
     }
 
     public static void listarCompromissos(){
     }
 
-    public List<String> getListaCompro() {
+    public static List<Compromisso> getListaCompro() {
         return listaCompro;
     }
 
-    public void setListaCompro(List<String> listaCompro) {
+    public void setListaCompro(List<Compromisso> listaCompro) {
         this.listaCompro = listaCompro;
     } 
     
