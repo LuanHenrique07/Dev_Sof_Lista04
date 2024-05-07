3041e8dfa6f43bbc8db50975f2509e5ff74b9bde
gabriel <gzanella202@gmail.com>
2024-05-06 21:40:20 -0300

    Inicio Ex5

---
+++ Cenario05/src/br/edu/up/view/IUsuario.java
@@ -0,0 +1,35 @@
+package view;
+
+import br.edu.up.Prompt;
+import model.Evento;
+import model.Reserva;
+
+public class IUsuario {
+
+        public void mostrarMenu() {
+        Prompt.imprimir("1. Adicionar evento");
+        Prompt.imprimir("2. Listar eventos");
+        Prompt.imprimir("3. Adicionar reserva");
+        Prompt.imprimir("4. Listar reservas");
+        Prompt.imprimir("5. Sair");
+    }
+
+    public void mostrarEvento(Evento evento) {
+        Prompt.imprimir("Nome: " + evento.getNome());
+        Prompt.imprimir("Data: " + evento.getData());
+        Prompt.imprimir("Local: " + evento.getLocal());
+        Prompt.imprimir("Lotação máxima: " + evento.getLotacaoMaxima());
+        Prompt.imprimir("Ingressos vendidos: " + evento.getIngressoVendido());
+        Prompt.imprimir("Preço do ingresso: " + evento.getPrecoIngresso());
+        Prompt.imprimir("");
+    }
+
+    public void mostrarReserva(Reserva reserva) {
+        Prompt.imprimir("Evento: " + reserva.getEvento().getNome());
+        Prompt.imprimir("Responsável: " + reserva.getResponsavel());
+        Prompt.imprimir("Quantidade de pessoas: " + reserva.getQuantidadePessoa());
+        Prompt.imprimir("Data da reserva: " + reserva.getDataReserva());
+        Prompt.imprimir("Valor total: " + reserva.getValorTotal());
+        Prompt.imprimir("");
+    }
+}
